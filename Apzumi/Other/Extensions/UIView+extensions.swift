ccf12e60d3790edaac5e929cb1f2e63eaa2fa9fb
Krzysztof Lech <krzysztof.lech@24abc.pl>
2018-09-11 11:51:23 +0200

    ListViewController - table view setup

---
+++ Apzumi/Other/Extensions/UIView+extensions.swift
@@ -0,0 +1,16 @@
+//
+//  UIView+extensions.swift
+//  Apzumi
+//
+//  Created by Krzysztof Lech on 11.09.2018.
+//  Copyright © 2018 Krzysztof Lech. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    static func toString() -> String {
+        return String(describing: self)
+    }
+}
--- Apzumi/ViewControllers/ListViewController.swift
+++ Apzumi/ViewControllers/ListViewController.swift
@@ -9,4 +9,32 @@
 import UIKit
 
 class ListViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.register(UINib(nibName: RepoTableViewCell.toString(), bundle: nil), forCellReuseIdentifier: RepoTableViewCell.toString())
+    }
+    
+    
+    
+}
+
+extension ListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: RepoTableViewCell.toString(), for: indexPath) as? RepoTableViewCell else { return UITableViewCell() }
+        
+        return cell
+    }
+}
+
+extension ListViewController: UITableViewDelegate {
+    
 }
